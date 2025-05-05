73ca42382d125c755aad90d877d200618a559925
Brandon Rojas <brandonrojaslameda@gmail.com>
2025-05-04 21:59:48 -0400

    Fix table header for hour on historical rates

--- views/admin/main.php
+++ views/admin/main.php
@@ -428,7 +428,7 @@ $current_page = $rates_data['current_page'];
                                             <svg class="w-4 h-4 text-gray-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"></path>
                                             </svg>
-                                            <span><?php _e('Hora', 'ves-converter'); ?></span>
+                                            <span><?php _e('Guardado', 'ves-converter'); ?></span>
                                         </div>
                                     </th>
                                 </tr>
